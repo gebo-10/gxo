5b90004a50dfb587ddfafc53c91c1101ea132c6c
gebo-10 <937319738@qq.com>
2019-05-17 13:38:10 +0800

    Update gpu.h

--- gxo/gxo/gpu.h
+++ gxo/gxo/gpu.h
@@ -1,5 +1,11 @@
 #ifndef _GXO_GPU_H
 #define _GXO_GPU_H
+include GPU type
 #include"gpu_buffer.h"
 #include"gpu_object.h"
+
+class GPU
+class GPUmemerymanager
+class GPUobjectmanager
+map objID GPUID
 #endif
